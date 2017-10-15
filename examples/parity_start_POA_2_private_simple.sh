0bf982b1a22765e6f3363117750a1e9f764466ee
viktor <baranov.viktor.27@gmail.com>
2017-10-15 16:00:20 +0300

    .

---
+++ examples/parity_start_POA_2_private_simple.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+parity --config examples/parity_config_POA_2_private_simple.toml --no-ui
---
+++ examples/parity_start_rinkeby.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+parity --config examples/parity_config_rinkeby.toml --warp --no-ui
