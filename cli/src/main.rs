103af3e3fa7bdf78aeddeb0e27d4a952e6edeb8e
Yurii Rashkovskii <yrashk@gmail.com>
2018-03-15 20:16:15 +0700

    Problem: complexity associated with error handling
    Currently, bridge will try to handle errors in some
    way in order to attempt to restore its functionality.
    
    However, this limits as to how the errors can be handled
    as every time a change in handling is needed, a patch
    for bridge will needed.
    
    Overtime, this will inevitably grow into a full-fledged
    supervisor.
    
    However, there are already supervisor programs out there
    (starting from all-encompassing systemd down to small
    supervision utilities)
    
    Solution: revert the handling of errors to the old behaviour
    but (very importantly) make bridge return meaningful error
    codes for all known error types so that the supervisor
    can make a proper decision as to what has to be done
    (restart, delayed restart, permanent shutdown, notification,
    etc.)

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -9,7 +9,7 @@ extern crate log;
 extern crate env_logger;
 extern crate bridge;
 
-use std::{env, fs};
+use std::{env, fs, io};
 use std::sync::Arc;
 use std::path::PathBuf;
 use docopt::Docopt;
@@ -21,6 +21,41 @@ use bridge::bridge::{create_bridge, create_deploy, Deployed};
 use bridge::config::Config;
 use bridge::error::{Error, ErrorKind};
 use bridge::web3;
+
+const ERR_UNKNOWN: i32 = 1;
+const ERR_IO_ERROR: i32 = 2;
+const ERR_CANNOT_CONNECT: i32 = 10;
+const ERR_CONNECTION_LOST: i32 = 11;
+const ERR_BRIDGE_CRASH: i32 = 11;
+
+pub struct UserFacingError(i32, Error);
+
+impl From<Error> for UserFacingError {
+	fn from(err: Error) -> Self {
+		UserFacingError(ERR_UNKNOWN, err)
+	}
+}
+
+impl From<String> for UserFacingError {
+	fn from(err: String) -> Self {
+		UserFacingError(ERR_UNKNOWN, err.into())
+	}
+}
+
+
+impl From<io::Error> for UserFacingError {
+	fn from(err: io::Error) -> Self {
+		UserFacingError(ERR_IO_ERROR, err.into())
+	}
+}
+
+
+impl From<(i32, Error)> for UserFacingError {
+	fn from((code, err): (i32, Error)) -> Self {
+		UserFacingError(code, err)
+	}
+}
+
 
 const USAGE: &'static str = r#"
 Ethereum-Kovan bridge.
@@ -46,16 +81,20 @@ fn main() {
 
 	match result {
 		Ok(s) => println!("{}", s),
-		Err(err) => print_err(err),
+		Err(UserFacingError(code, err)) => {
+			print_err(err);
+			::std::process::exit(code);
+		},
 	}
 }
+
 
 fn print_err(err: Error) {
 	let message = err.iter().map(|e| e.to_string()).collect::<Vec<_>>().join("\n\nCaused by:\n  ");
 	println!("{}", message);
 }
 
-fn execute<S, I>(command: I) -> Result<String, Error> where I: IntoIterator<Item=S>, S: AsRef<str> {
+fn execute<S, I>(command: I) -> Result<String, UserFacingError> where I: IntoIterator<Item=S>, S: AsRef<str> {
 	info!(target: "bridge", "Parsing cli arguments");
 	let args: Args = Docopt::new(USAGE)
 		.and_then(|d| d.argv(command).deserialize()).map_err(|e| e.to_string())?;
@@ -67,14 +106,12 @@ fn execute<S, I>(command: I) -> Result<String, Error> where I: IntoIterator<Item
 	let mut event_loop = Core::new().unwrap();
 
 	info!(target: "bridge", "Establishing ipc connection");
-	let app = loop {
-		match App::new_ipc(config.clone(), &args.arg_database, &event_loop.handle()) {
-			Ok(app) => break app,
+	let app = match App::new_ipc(config.clone(), &args.arg_database, &event_loop.handle()) {
+			Ok(app) => app,
 			Err(e) => {
-				warn!("Can't establish an IPC connection, will attempt to reconnect: {:?}", e);
-				::std::thread::sleep(::std::time::Duration::from_secs(1));
+				warn!("Can't establish an IPC connection: {:?}", e);
+				return Err((ERR_CANNOT_CONNECT, e).into());
 			},
-		}
 	};
 	let app_ref = Arc::new(app.as_ref());
 
@@ -96,32 +133,22 @@ fn execute<S, I>(command: I) -> Result<String, Error> where I: IntoIterator<Item
 
 	info!(target: "bridge", "Starting listening to events");
 	let bridge = create_bridge(app_ref.clone(), &database).and_then(|_| future::ok(true)).collect();
-	let mut result = event_loop.run(bridge);
-	loop {
-		result = match &result {
-			&Err(Error(ErrorKind::Web3(web3::error::Error(web3::error::ErrorKind::Io(ref e), _)), _)) if e.kind() == ::std::io::ErrorKind::BrokenPipe => {
-			    warn!("Connection to a node has been severed, attempting to reconnect");
-				let app = match App::new_ipc(config.clone(), &args.arg_database, &event_loop.handle()) {
-					Ok(app) => {
-						warn!("Connection has been re-established, restarting");
-						app
-					},
-					_ => {
-						::std::thread::sleep(::std::time::Duration::from_secs(1));
-						continue
-					},
-				};
-				let app_ref = Arc::new(app.as_ref());
-				let bridge = create_bridge(app_ref.clone(), &database).and_then(|_| future::ok(true)).collect();
-				event_loop.run(bridge)
+	let result = event_loop.run(bridge);
+	match result {
+			Err(Error(ErrorKind::Web3(web3::error::Error(web3::error::ErrorKind::Io(e), _)), _)) => {
+				if e.kind() == ::std::io::ErrorKind::BrokenPipe {
+					warn!("Connection to a node has been severed");
+					return Err((ERR_CONNECTION_LOST, e.into()).into());
+				} else {
+					warn!("I/O error: {:?}", e);
+					return Err((ERR_IO_ERROR, e.into()).into());
+				}
 			},
-			&Err(ref e) => {
-				warn!("Bridge is down with {}, attempting to restart", e);
-				let bridge = create_bridge(app_ref.clone(), &database).and_then(|_| future::ok(true)).collect();
-				event_loop.run(bridge)
+			Err(e) => {
+				warn!("Bridge crashed with {}", e);
+				return Err((ERR_BRIDGE_CRASH, e).into());
 			},
-			&Ok(_) => break,
-		};
+			Ok(_) => (),
 	}
 
 	Ok("Done".into())
